1297fbe4d93c1d17accdb7c60faa3572955e7c95
zhaohuijun <18766289420@163.com>
2023-07-31 11:56:02 +0800

    feat: https://gitee.com/junyue/cxygzl-pro-develop/issues/I7P7OE

--- core-app/src/main/java/com/cxygzl/core/node/impl/AssignUserFormDeptStrategyImpl.java
+++ core-app/src/main/java/com/cxygzl/core/node/impl/AssignUserFormDeptStrategyImpl.java
@@ -1,12 +1,17 @@
 package com.cxygzl.core.node.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.Convert;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.cxygzl.common.constants.NodeUserTypeEnum;
 import com.cxygzl.common.constants.ProcessInstanceConstant;
+import com.cxygzl.common.dto.R;
 import com.cxygzl.common.dto.flow.Node;
 import com.cxygzl.common.dto.flow.NodeUser;
 import com.cxygzl.common.utils.CommonUtil;
 import com.cxygzl.core.node.AssignUserStrategy;
+import com.cxygzl.core.utils.CoreHttpUtil;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
@@ -16,13 +21,13 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * 来自表单
+ * 来自表单部门
  * @author Huijun Zhao
  * @description
- * @date 2023-07-07 13:42
+ * @date 2023-07-31 11:30
  */
 @Component
-public class AssignUserFormStrategyImpl implements InitializingBean, AssignUserStrategy {
+public class AssignUserFormDeptStrategyImpl implements InitializingBean, AssignUserStrategy {
     @Override
     public List<String> handle(Node node, NodeUser rootUser, Map<String, Object> variables) {
 
@@ -40,9 +45,18 @@ public class AssignUserFormStrategyImpl implements InitializingBean, AssignUserS
             String jsonString = JSON.toJSONString(variable);
             List<NodeUser> nodeUserDtoList = CommonUtil.toArray(jsonString, NodeUser.class);
 
-            List<String> userIdList = nodeUserDtoList.stream().map(w -> String.valueOf(w.getId())).collect(Collectors.toList());
+            List<String> deptIdList = nodeUserDtoList.stream().map(w -> String.valueOf(w.getId())).collect(Collectors.toList());
+//部门id
 
-            assignList.addAll(userIdList);
+            if (CollUtil.isNotEmpty(deptIdList)) {
+
+                R<List<String>> r= CoreHttpUtil.queryUserIdListByDepIdList(deptIdList);
+
+                List<String> data = r.getData();
+                if (CollUtil.isNotEmpty(data)) {
+                    assignList.addAll(data);
+                }
+            }
 
         }
         return assignList;
@@ -50,7 +64,7 @@ public class AssignUserFormStrategyImpl implements InitializingBean, AssignUserS
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        afterPropertiesSet(ProcessInstanceConstant.AssignedTypeClass.FORM_USER);
+        afterPropertiesSet(ProcessInstanceConstant.AssignedTypeClass.FORM_DEPT);
 
     }
 }
