16295ede203b58fff8ed4f7fb4c5f9b8e21426a4
zhaohuijun <18766289420@163.com>
2023-11-07 16:04:19 +0800

    feat: 测试动态表单选项

--- biz-app/src/main/java/com/cxygzl/biz/controller/TestController.java
+++ biz-app/src/main/java/com/cxygzl/biz/controller/TestController.java
@@ -7,6 +7,7 @@ import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import com.cxygzl.common.dto.R;
+import com.cxygzl.common.dto.flow.SelectValue;
 import com.cxygzl.common.utils.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,53 @@ import java.util.List;
 public class TestController {
 
 
+
+    @PostMapping("dynamicForm1")
+    public Object dynamicForm1(@RequestBody String s,@RequestHeader(required = false) String b){
+        log.info("收到动态表单数据1：{} {}",s,b);
+        HashMap<Object, Object> data = new HashMap<>();
+        com.alibaba.fastjson2.JSONObject jsonObject = JsonUtil.parseObject(s);
+        Integer number = jsonObject.getInteger("number");
+        String selectVal = jsonObject.getString("selectVal");
+        if(number==null){
+            data.put("perm","H");
+            return data;
+        }
+        data.put("perm","E");
+        List<SelectValue> selectValueList=new ArrayList<>();
+        if(StrUtil.isNotBlank(selectVal)){
+            List<SelectValue> t = JsonUtil.parseArray(selectVal, SelectValue.class);
+            selectValueList.addAll(t);
+        }
+        if(number>10){
+            data.put("value","");
+            List list=new ArrayList();
+            {
+                Dict set = Dict.create().set("key", "a1").set("value", "水电费");
+                list.add(set);
+            }
+            if(selectValueList.stream().filter(w->StrUtil.equals(w.getKey(),"a1")).count()>0){
+                data.put("value",selectValueList);
+
+            }
+            data.put("options",list);
+        }else{
+            data.put("value","");
+            List list=new ArrayList();
+            {
+                Dict set = Dict.create().set("key", "a2").set("value", "推荐人体");
+                list.add(set);
+            }
+            if(selectValueList.stream().filter(w->StrUtil.equals(w.getKey(),"a2")).count()>0){
+                data.put("value",selectValueList);
+
+            }
+            data.put("options",list);
+        }
+
+
+        return  (data);
+    }
 
     @PostMapping("dynamicForm")
     public Object dynamicForm(@RequestBody String s,@RequestHeader(required = false) String b){
