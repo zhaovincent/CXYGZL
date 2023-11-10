91a6f6fa209da89bb4e6d8c53b1d388e8d574b1f
zhaohuijun <18766289420@163.com>
2023-11-10 15:24:17 +0800

    feat: 抄送节点支持多选配置

--- core-app/src/main/java/com/cxygzl/core/servicetask/CopyServiceTask.java
+++ core-app/src/main/java/com/cxygzl/core/servicetask/CopyServiceTask.java
@@ -1,15 +1,11 @@
 package com.cxygzl.core.servicetask;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.convert.Convert;
-import cn.hutool.core.util.StrUtil;
-import com.cxygzl.common.constants.NodeUserTypeEnum;
 import com.cxygzl.common.constants.ProcessInstanceConstant;
 import com.cxygzl.common.dto.ProcessInstanceCopyDto;
-import com.cxygzl.common.dto.R;
 import com.cxygzl.common.dto.flow.Node;
 import com.cxygzl.common.dto.flow.NodeUser;
 import com.cxygzl.common.utils.JsonUtil;
+import com.cxygzl.core.node.AssignUserStrategyFactory;
 import com.cxygzl.core.node.NodeDataStoreFactory;
 import com.cxygzl.core.utils.BizHttpUtil;
 import org.flowable.engine.delegate.DelegateExecution;
@@ -19,7 +15,6 @@ import org.flowable.engine.impl.persistence.entity.ExecutionEntityImpl;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * 抄送任务处理器--java服务任务
@@ -34,50 +29,40 @@ public class CopyServiceTask implements JavaDelegate {
 
         Node node = NodeDataStoreFactory.getInstance().getNode(flowId, nodeId);
 
+//发起人
+        Object rootUserObj = execution.getVariable(ProcessInstanceConstant.VariableKey.STARTER);
+        NodeUser rootUser = JsonUtil.parseArray(JsonUtil.toJSONString(rootUserObj), NodeUser.class).get(0);
+        String rootUserId = rootUser.getId();
+        if (node != null) {
+
+            Map<String, Object> variables = execution.getVariables();
+
+            Integer assignedType = node.getAssignedType();
+
+            List<String> userIdList = AssignUserStrategyFactory.getStrategy(assignedType).handle(node, rootUser, variables);
 
 
-        //指定人员
-        List<NodeUser> userDtoList = node.getNodeUserList();
-        //用户id
-        List<String> userIdList = userDtoList.stream().filter(w -> StrUtil.equals(w.getType(), NodeUserTypeEnum.USER.getKey())).map(w -> Convert.toStr(w.getId())).collect(Collectors.toList());
-        //部门id
-        List<String> deptIdList = userDtoList.stream().filter(w -> StrUtil.equals(w.getType(), NodeUserTypeEnum.DEPT.getKey())).map(w -> Convert.toStr(w.getId())).collect(Collectors.toList());
 
-        if (CollUtil.isNotEmpty(deptIdList)) {
+            for (String userIds : userIdList) {
+                //发送抄送任务
+                ProcessInstanceCopyDto processInstanceCopyDto = new ProcessInstanceCopyDto();
+                processInstanceCopyDto.setNodeTime(new Date());
+                processInstanceCopyDto.setStartUserId((rootUser.getId()));
+                processInstanceCopyDto.setFlowId(flowId);
+                processInstanceCopyDto.setProcessInstanceId(execution.getProcessInstanceId());
+                processInstanceCopyDto.setNodeId(nodeId);
+                processInstanceCopyDto.setNodeName(node.getNodeName());
+                processInstanceCopyDto.setFormData(JsonUtil.toJSONString(variables));
+                processInstanceCopyDto.setUserId((userIds));
 
-            R<List<String>> r = BizHttpUtil.queryUserIdListByDepIdList(deptIdList);
-
-            List<String> data = r.getData();
-            if (CollUtil.isNotEmpty(data)) {
-                for (String datum : data) {
-                    if (!userIdList.contains(datum)) {
-                        userIdList.add(datum);
-                    }
-                }
+                BizHttpUtil.saveCC(processInstanceCopyDto);
             }
         }
 
 
-        //发起人
-        Object rootUserObj = execution.getVariable(ProcessInstanceConstant.VariableKey.STARTER);
-        NodeUser rootUser = JsonUtil.parseArray(JsonUtil.toJSONString(rootUserObj), NodeUser.class).get(0);
 
-        Map<String, Object> variables = execution.getVariables();
 
-        for (String userIds : userIdList) {
-            //发送抄送任务
-            ProcessInstanceCopyDto processInstanceCopyDto = new ProcessInstanceCopyDto();
-            processInstanceCopyDto.setNodeTime(new Date());
-            processInstanceCopyDto.setStartUserId((rootUser.getId()));
-            processInstanceCopyDto.setFlowId(flowId);
-            processInstanceCopyDto.setProcessInstanceId(execution.getProcessInstanceId());
-            processInstanceCopyDto.setNodeId(nodeId);
-            processInstanceCopyDto.setNodeName(node.getNodeName());
-            processInstanceCopyDto.setFormData(JsonUtil.toJSONString(variables));
-            processInstanceCopyDto.setUserId((userIds));
 
-            BizHttpUtil.saveCC(processInstanceCopyDto);
-        }
 
 
     }
